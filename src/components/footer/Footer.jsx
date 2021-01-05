9bb655a2d99562cecc92624df6d1391ba1b4aef1
Arman Khilko <arma.biz9805@gmail.com>
2021-01-05 15:18:42 +0400

    Create: initial footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,3 +1,13 @@
-const Footer = () => <></>;
+import Container from "_components/container";
+
+import "./Footer.scss";
+
+const Footer = () => (
+    <footer className="footer">
+        <Container className="footer--inner">
+
+        </Container>
+    </footer>
+);
 
 export default Footer;
