a43df9bfc97394eda0d7e2e488baa2c71a771f30
Arman Khilko <arma.biz9805@gmail.com>
2021-01-05 17:53:52 +0400

    Add: page routes

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,15 +2,30 @@ import { createBrowserHistory } from "history";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Frame from "_components/frame";
+import Splash from "_pages/splash";
+import Guides from "_pages/guides";
+import Reference from "_pages/reference";
+import Contribute from "_pages/contribute";
+import Analyze from "_pages/analyze";
+import Donate from "_pages/donate";
+import PageNotFound from "_pages/error/404";
 
 const history = createBrowserHistory();
 
 // TODO: change path to constants
 const Routes = () => (
     <BrowserRouter history={history}>
-        <Switch>
-            <Route exact path="/" component={Frame} />
-        </Switch>
+        <Frame>
+            <Switch>
+                <Route exact path="/" component={Splash} />
+                <Route exact path="/guides" component={Guides} />
+                <Route exact path="/reference" component={Reference} />
+                <Route exact path="/contribute" component={Contribute} />
+                <Route exact path="/analyze" component={Analyze} />
+                <Route exact path="/donate" component={Donate} />
+                <Route exact path="**" component={PageNotFound} />
+            </Switch>
+        </Frame>
     </BrowserRouter>
 );
 
