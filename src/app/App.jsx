12d8c22d717b2aefc27405b55a614d5ea146f556
Arman Khilko <arma.biz9805@gmail.com>
2021-01-05 12:17:58 +0400

    Include: logo and initial layout

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,10 +1,15 @@
 import { hot } from "react-hot-loader/root";
+import Logo from "_components/logo";
+import Container from "_components/container";
 
 import "normalize.css";
 import "_theme/main.scss";
 
 const App = () => (
     <React.StrictMode>
+        <Container>
+            <Logo />
+        </Container>
     </React.StrictMode>
 );
 
