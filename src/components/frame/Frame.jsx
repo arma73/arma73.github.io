1d78f8f695859d74a0b2a3918b67b205a31fcc08
Arman Khilko <arma.biz9805@gmail.com>
2021-01-05 18:34:21 +0400

    Update: toggling the header theme based on route

--- src/components/frame/Frame.jsx
+++ src/components/frame/Frame.jsx
@@ -1,12 +1,19 @@
+import { withRouter } from "react-router-dom";
 import Header from "_components/layouts/header";
 import Footer from "_components/layouts/footer";
 
-const Frame = ({ children }) => (
-    <div className="frame">
-        <Header />
-        {children}
-        <Footer />
-    </div>
-);
+const Frame = ({ children, location }) => {
+    const isIndexRoute = location.pathname === "/";
 
-export default Frame;
+    return (
+        <div className="frame">
+            <Header 
+                theme={isIndexRoute ? "light" : "dark"}
+            />
+            {children}
+            <Footer />
+        </div>
+    );
+};
+
+export default withRouter(Frame);
