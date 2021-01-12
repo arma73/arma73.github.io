77bf3b7eb949f24a449f208d4ce2c7708b529538
Arman Khilko <arma.biz9805@gmail.com>
2021-01-12 22:23:41 +0400

    Change: remove core js and add @babel/preset-env in development

--- configs/babel/presets.js
+++ configs/babel/presets.js
@@ -1,20 +1,26 @@
 "use-strict";
 
-module.exports = ({ production, loose, spec, useBuiltIns, modules }) => {
+module.exports = ({ development, production, loose, spec, modules }) => {
     const configs = [
-        "@babel/preset-react"
+        [
+            "@babel/preset-react",
+            {
+                "runtime": "automatic"
+            }
+        ]
     ];
 
+    development && 
+        configs.unshift("@babel/preset-env");
+
     production &&
-        configs.push([
-            "@babel/env",
+        configs.unshift([
+            "@babel/preset-env",
             {
                 spec,
                 loose,
                 modules,
-                useBuiltIns,
                 "debug": false,
-                "corejs": "3",
             }
         ]);
 
