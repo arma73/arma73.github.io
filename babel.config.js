8f7edca58e8841fd149dc1468e7949920ccada59
Arman Khilko <arma.biz9805@gmail.com>
2021-01-12 19:17:33 +0400

    Change: do not use for building core js

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,8 @@ const presets = require("./configs/babel/presets");
 const plugins = require("./configs/babel/plugins");
 
 module.exports = api => {
-    const web = api.caller(caller => Boolean(caller && caller.target === "web"));
+    // NOTE: when you change the babel config, 
+    //   you need to delete the cache for the new changes to work.
     const babel = api.caller(caller => Boolean(caller && caller.name === "babel-loader"));
 
     const production = api.env("production");
@@ -13,11 +14,10 @@ module.exports = api => {
     const legacy = true;
     const spec = true; // specification, makes the code slower but more reliable.
     const loose = false; // makes the code faster, but deviate from the standard.
-    const useBuiltIns = web ? "usage" : undefined;
     const modules = babel ? false : "commonjs";
 
     return {
-        "presets": presets({ development, production, loose, spec, useBuiltIns, modules }),
+        "presets": presets({ development, production, loose, spec, modules }),
         "plugins": plugins({ development, production, loose, legacy }),
     };
 };
