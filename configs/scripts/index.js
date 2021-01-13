58f6fd5b07b3681bf0c59b40d039be9d0ba48114
Arman Khilko <arma.biz9805@gmail.com>
2021-01-13 16:44:43 +0400

    Change: entry point for assembly

--- configs/scripts/index.js
+++ configs/scripts/index.js
@@ -5,11 +5,11 @@ const { merge } = require("webpack-merge");
 
 // Helpers
 const { environment } = require("../helpers/env");
+const { ssg } = require("../helpers/options");
 
 // Webpacks
 const common = require("../webpack/common.config");
-const config = require(`../webpack/${environment}.config`);
 
+const config = require(`../webpack/${ssg ? "ssg" : environment}.config`);
 const mergeConfig = merge(common, config);
-
 require(`./${environment}`)(mergeConfig);
