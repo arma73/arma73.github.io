d3cb4334986084a6c48ec4591412fc20637d0e76
Arman Khilko <arma.biz9805@gmail.com>
2021-01-13 16:41:54 +0400

    Add: config for static site generator

---
+++ configs/webpack/ssg.config.js
@@ -0,0 +1,19 @@
+"use-strict";
+
+// Core
+const { merge } = require("webpack-merge");
+
+// Parts
+const optimization = require("../parts/ssg/optimization");
+const entry = require("../parts/ssg/entry");
+const output = require("../parts/ssg/output");
+
+// Plugins
+const plugins = require("../plugins/ssg");
+
+module.exports = merge(
+    entry,
+    output,
+    optimization,
+    plugins,
+);
