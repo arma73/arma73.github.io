243290a986ce2fd219031c09894fc6604300c3f2
Arman Khilko <arma.biz9805@gmail.com>
2021-01-13 20:22:56 +0400

    Add: markdown to loaders

--- configs/presets/rules.js
+++ configs/presets/rules.js
@@ -5,10 +5,12 @@ const scripts = require("./scripts");
 const styles = require("./styles");
 const fonts = require("./fonts");
 const images = require("./images");
+const markdown = require("./markdown");
 
 module.exports = [
     ...scripts(),
     ...styles(),
     ...fonts(),
-    ...images()
+    ...images(),
+    ...markdown()
 ];
