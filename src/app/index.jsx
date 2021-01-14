a31883d8556005e008088f07e28a9754550a1afb
Arman Khilko <arma.biz9805@gmail.com>
2021-01-14 11:47:40 +0400

    Change: entry point for web application

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,5 +1,10 @@
-import { render } from "react-dom";
+import ReactDOM from "react-dom";
+import isClient from "_utils/isClient";
 
 import App from "./App";
 
-render(<App />, document.querySelector("#app"));
+const render = __PROD__ ? ReactDOM.hydrate : ReactDOM.render;
+
+if (isClient) {
+    render(<App />, document.querySelector("#app"));
+}
