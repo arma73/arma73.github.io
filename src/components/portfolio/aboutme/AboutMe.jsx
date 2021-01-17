f83f4881410bfd707ab1a27936c8a1da39188beb
Arman Khilko <arma.biz9805@gmail.com>
2021-01-17 22:24:52 +0400

    Change: aboutme component remove attributes

--- src/components/portfolio/aboutme/AboutMe.jsx
+++ src/components/portfolio/aboutme/AboutMe.jsx
@@ -2,14 +2,14 @@ import "./AboutMe.scss";
 
 const AboutMe = () => (
     <div className="text-like-box w-clearfix">
-        <div className="text" data-ix="arrow-load">A</div>
-        <div className="straight-line" data-ix="arrow-load"></div>
-        <div className="straight-line-full" data-ix="loading-line-animation"></div>
-        <div className="straight-line-full" data-ix="loading-line-animation-2"></div>
-        <div className="straight-line-full" data-ix="loading-line-animation-3"></div>
-        <div className="straight-line-full" data-ix="loading-line-animation-6-long"></div>
-        <div className="straight-line-full" data-ix="loading-line-animation-5"></div>
-        <div className="shortened straight-line-full" data-ix="loading-line-animation-6shorted"></div>
+        <div className="text">Arman</div>
+        <div className="straight-line"></div>
+        <div className="straight-line-full"></div>
+        <div className="straight-line-full"></div>
+        <div className="straight-line-full"></div>
+        <div className="straight-line-full"></div>
+        <div className="straight-line-full"></div>
+        <div className="shortened straight-line-full"></div>
     </div>
 );
 
