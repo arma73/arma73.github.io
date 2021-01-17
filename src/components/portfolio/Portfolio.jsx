a6957b99a9a81e8e11abf7df4cc2004ef803cc7e
Arman Khilko <arma.biz9805@gmail.com>
2021-01-17 21:31:44 +0400

    Create: aboutme section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,12 +1,15 @@
 import TopBar from "./topbar";
+import AboutMe from "./aboutme";
 
 import "./Portfolio.scss";
+import "./Base.scss";
 
 const Portfolio = () => (
     <div className="portfolio--layout">
         <div className="portfolio--illustration preserve-3d">
             <div className="portfolio">
                 <TopBar />
+                <AboutMe />
             </div>
         </div>
     </div>
