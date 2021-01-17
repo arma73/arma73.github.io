bf22544f3a6d7972b716d565a65ba9e22300b315
Arman Khilko <arma.biz9805@gmail.com>
2021-01-17 19:40:43 +0400

    Change: initial view logo

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
-import Icon from "_components/icon";
+import IconLogo from "_theme/icons/logo-default.svg";
 
 import "./Logo.scss";
 
 const Logo = ({ theme = "light" }) => (
     <Link to="/" className={`logo logo--${theme}`}> 
-        <Icon theme={theme} depth={50} />
-        <span className="logo__text"> amollo </span>
+        <IconLogo  
+            width={"35px"}
+        />
+        <span className="logo__text"> arma73 </span>
     </Link>
 );
 
