d487191ed9a33bd6e693c847aa1b1dc85eadf7a8
Arman Khilko <arma.biz9805@gmail.com>
2021-01-17 22:25:55 +0400

    Create: mountainphoto component

---
+++ src/components/portfolio/mountainphoto/MountainPhoto.jsx
@@ -0,0 +1,16 @@
+import "./MountainPhoto.scss";
+
+const MountainPhoto = () => (
+    <div className="mountain-photo-parent">
+        <div className="mountain-photo__real-deal">
+            <div className="mountain"></div>
+            <div className="_2 mountain"></div>
+        </div>
+        <div className="arrow-line">
+            <div className="arrow-line-small"></div>
+            <div className="arrow-line-small v2"></div>
+        </div>
+    </div>
+);
+
+export default MountainPhoto;
