5beda09a62c16109b36f5cd967291b432aeaa927
arma73 <arma.biz9805@gmail.com>
2021-01-26 00:23:07 +0400

    Add: initial route to blog

--- src/components/blogtower/BlogTower.jsx
+++ src/components/blogtower/BlogTower.jsx
@@ -1,5 +1,7 @@
+import { Link } from "react-router-dom";
 import clsx from "clsx";
 import { useHover } from "_hooks";
+import { BLOG_PAGE_ROUTE } from "_settings/path.routes";
 import Section from "_components/form/Section.jsx";
 import Tooltip from "_components/tooltip";
 
@@ -11,9 +13,9 @@ const BlogTower = () => {
     return (
         <Section className="blog--tower">
             <div className="box--blog">
-                <div className="layer preserve-3d" {...bind}>
+                <Link to={BLOG_PAGE_ROUTE} className="layer preserve-3d" {...bind}>
                     <div className="layer__top-bar" />
-                </div>
+                </Link>
                 <div className={clsx("layer__square-1", "preserve-3d", { "square-1__hovered": isHovered })} />
                 <div className={clsx("layer__square-2", "preserve-3d", { "square-2__hovered": isHovered })} />
                 <div className="layer__square-bottom" />
--- src/routes/Site.routes.jsx
+++ src/routes/Site.routes.jsx
@@ -1,6 +1,7 @@
 import { Switch, Route, Redirect } from "react-router-dom";
-import { MAIN_PAGE_ROUTE } from "_settings/path.routes";
+import { MAIN_PAGE_ROUTE, BLOG_PAGE_ROUTE } from "_settings/path.routes";
 import Main from "_pages/main";
+import Blog from "_pages/blog";
 
 const SiteRoutes = () => (
     <Switch>
@@ -11,6 +12,7 @@ const SiteRoutes = () => (
             render={({ location }) => <Redirect to={`${location.pathname}/`} />}
         />
         <Route exact path={MAIN_PAGE_ROUTE} component={Main} />
+        <Route exact path={BLOG_PAGE_ROUTE} component={Blog} />
     </Switch>
 );
 
