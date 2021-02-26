0bf3a174734f4862122aa821b898353d6310fb7b
arma73 <arma.biz9805@gmail.com>
2021-02-26 08:31:40 +0400

    update: static convert to tsx

--- src/server/Static.tsx
+++ src/server/Static.tsx
@@ -1,9 +1,12 @@
 import ReactDOMServer from "react-dom/server";
 import { StaticRouter } from "react-router-dom";
 import Markup from "_components/markup";
+import { Content } from "_interfaces/content.interface";
 
-const Static = locals => {
-    const renderedHtml = ReactDOMServer.renderToString(
+type StaticSignature = (locals: Content) => string;
+
+const Static: StaticSignature = locals => {
+    const renderedHtml: string = ReactDOMServer.renderToString(
         <StaticRouter location={locals.path} context={{}}>
             <Markup locals={locals} />
         </StaticRouter>
