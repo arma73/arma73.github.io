85dcceab358db5e7953a54dd0039626496da1a4d
arma73 <arma.biz9805@gmail.com>
2021-02-27 11:06:24 +0400

    update: root dom id

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,11 @@
 import ReactDOM from "react-dom";
 import { isClient } from "_utils/isClient";
+import { ROOT_DOM_ID } from "_settings/constants";
 
 import App from "./App";
 
 const render = __PROD__ ? ReactDOM.hydrate : ReactDOM.render;
 
 if (isClient) {
-    render(<App />, document.querySelector("#app"));
+    render(<App />, document.querySelector(`#${ROOT_DOM_ID}`));
 }
