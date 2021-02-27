d808aa61b1632bce64629f6a2a892771cea97a69
arma73 <arma.biz9805@gmail.com>
2021-02-27 11:08:33 +0400

    fix: locals signature

--- src/server/Static.tsx
+++ src/server/Static.tsx
@@ -1,9 +1,19 @@
 import ReactDOMServer from "react-dom/server";
+import { Compilation } from "webpack";
 import { StaticRouter } from "react-router-dom";
 import Markup from "_components/markup";
 import { Content } from "_interfaces/content.interface";
 
-type StaticSignature = (locals: Content) => string;
+export interface Locals {
+    "content": Content;
+    "path": string;
+    "assets": { [K: string]: string };
+    "webpackStats": {
+        "compilation": Compilation;
+    };
+}
+
+type StaticSignature = (locals: Locals) => string;
 
 const Static: StaticSignature = locals => {
     const renderedHtml: string = ReactDOMServer.renderToString(
