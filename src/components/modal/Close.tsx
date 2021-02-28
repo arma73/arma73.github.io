c2aae256fbcb1b94b513bbccdcde174810250bf5
arma73 <arma.biz9805@gmail.com>
2021-02-28 11:37:37 +0400

    create: initial modal component

---
+++ src/components/modal/Close.tsx
@@ -0,0 +1,24 @@
+import { ReactNode, FC, MouseEvent, SVGProps } from "react";
+import MdClose from "_theme/icons/close.svg";
+import "./Close.scss";
+
+interface CloseProps {
+    "children"?: ReactNode;
+    "onClick": (event: MouseEvent<HTMLSpanElement>) => void;
+    "color"?: string;
+    "icon"?: FC<SVGProps<SVGSVGElement>>;
+}
+
+const Close: FC<CloseProps> = ({
+    children = null,
+    color = "#505050",
+    onClick,
+    icon = MdClose,
+}) => (
+    <span className="closeModal" style={{ color }} onClick={onClick}>
+        {icon}
+        {children}
+    </span>
+);
+
+export default Close;
