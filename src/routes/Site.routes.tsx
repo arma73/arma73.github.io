6c36cb1dca8cbc1410dba3ef07541c46ba77bffa
arma73 <arma.biz9805@gmail.com>
2021-03-01 07:28:14 +0400

    update: with showcase routes

--- src/routes/Site.routes.tsx
+++ src/routes/Site.routes.tsx
@@ -1,8 +1,15 @@
 import { FC } from "react";
-import { Switch, Route, Redirect, RouteComponentProps } from "react-router-dom";
+import {
+    Switch,
+    Route,
+    Redirect,
+    RouteComponentProps,
+    useLocation,
+} from "react-router-dom";
 import { RoutePage } from "_settings/path.routes";
 import ContentRoutes from "./Content.routes";
 import Main from "_pages/main";
+import ShowcaseRoutes from "./Showcase.routes";
 import Articles from "_pages/articles";
 import { DynamicImportType } from "_interfaces/dynamic.interface";
 
@@ -10,20 +17,30 @@ export interface ContentDynamicImport {
     "importContent": (path: string) => DynamicImportType<string>;
 }
 
-const SiteRoutes: FC<RouteComponentProps & ContentDynamicImport> = props => (
-    <Switch>
-        <Route
-            exact
-            strict
-            path="/:url*"
-            render={({ location }) => <Redirect to={`${location.pathname}/`} />}
-        />
-        <Route exact path={RoutePage.MAIN} component={Main} />
-        <Route exact path={RoutePage.ARTICLES} component={Articles} />
-        <Route>
-            <ContentRoutes {...props} />
-        </Route>
-    </Switch>
-);
+const SiteRoutes: FC<RouteComponentProps & ContentDynamicImport> = props => {
+    const location = useLocation();
+    const showcase = location.state && location.state.showcase;
+
+    return (
+        <>
+            <Switch location={showcase || location}>
+                <Route
+                    exact
+                    strict
+                    path="/:url*"
+                    render={({ location }) => (
+                        <Redirect to={`${location.pathname}/`} />
+                    )}
+                />
+                <Route exact path={RoutePage.MAIN} component={Main} />
+                <Route exact path={RoutePage.ARTICLES} component={Articles} />
+                <Route>
+                    <ContentRoutes {...props} />
+                </Route>
+            </Switch>
+            {showcase && <ShowcaseRoutes />}
+        </>
+    );
+};
 
 export default SiteRoutes;
