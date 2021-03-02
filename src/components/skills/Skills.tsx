26790ba741a8b34aed9c8ddc400f6f618ca12f99
arma73 <arma.biz9805@gmail.com>
2021-03-02 12:47:04 +0400

    change: skills and social places

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -1,10 +1,26 @@
+import { useHover } from "_hooks/useHover";
+import Section from "_components/form/Section";
 import { RiseAnimated } from "_components/animated";
+import Tooltip from "_components/tooltip";
+
 import "./Skills.scss";
 
-const Skills = () => (
-    <RiseAnimated toX="-4px" toY="-4px">
-        <div className="skills" />
-    </RiseAnimated>
-);
+const Skills = () => {
+    const [isHovered, bind] = useHover();
 
+    return (
+        <Section className="skills">
+            <div className="box--skills" {...bind}>
+                <RiseAnimated toX="-4px" toY="-4px">
+                    <div className="circles">
+                        <div className="circle-top preserve-3d"></div>
+                        <div className="circle-top preserve-3d"></div>
+                        <div className="circle-top"></div>
+                    </div>
+                </RiseAnimated>
+            </div>
+            <Tooltip show={isHovered} text="Skills" />
+        </Section>
+    );
+};
 export default Skills;
--- src/components/social/Social.tsx
+++ src/components/social/Social.tsx
@@ -1,26 +1,10 @@
-import { useHover } from "_hooks/useHover";
-import Section from "_components/form/Section";
 import { RiseAnimated } from "_components/animated";
-import Tooltip from "_components/tooltip";
-
 import "./Social.scss";
 
-const Social = () => {
-    const [isHovered, bind] = useHover();
+const Social = () => (
+    <RiseAnimated toX="-4px" toY="-4px">
+        <div className="social" />
+    </RiseAnimated>
+);
 
-    return (
-        <Section className="social">
-            <div className="box--social" {...bind}>
-                <RiseAnimated toX="-4px" toY="-4px">
-                    <div className="circles">
-                        <div className="circle-top preserve-3d"></div>
-                        <div className="circle-top preserve-3d"></div>
-                        <div className="circle-top"></div>
-                    </div>
-                </RiseAnimated>
-            </div>
-            <Tooltip show={isHovered} text="Social" />
-        </Section>
-    );
-};
 export default Social;
