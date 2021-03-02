e877e4c87e2e8cbc3e83c3b8f23b87d5212c342e
arma73 <arma.biz9805@gmail.com>
2021-03-02 11:48:14 +0400

    update: about me section telephone with link

--- src/pages/aboutme/AboutMe.tsx
+++ src/pages/aboutme/AboutMe.tsx
@@ -25,7 +25,8 @@ const PageAboutMe = () => (
             </S.ContactItem>
             <S.ContactItem>
                 {" "}
-                <IconTelephone width="20px" /> +37498220530{" "}
+                <IconTelephone width="20px" />{" "}
+                <a href="tel:+374-98-220-530">+374-98-220-530</a>{" "}
             </S.ContactItem>
             <S.ContactItem>
                 {" "}
