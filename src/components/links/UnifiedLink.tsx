9966aa525d7990ac06ff700270a450b300c91a82
arma73 <arma.biz9805@gmail.com>
2021-03-02 11:44:34 +0400

    create: base link components

---
+++ src/components/links/UnifiedLink.tsx
@@ -0,0 +1,34 @@
+import { ReactNode } from "react";
+import { Link } from "react-router-dom";
+
+const isExternalLink = (url: string) =>
+    typeof url === "string" &&
+    (url.startsWith("http") ||
+        url.startsWith("mailto") ||
+        url.startsWith("tel:"));
+
+interface UnifiedLinkProps {
+    "children": ReactNode;
+    "to": string;
+    "className"?: string;
+    "target"?: "_blank";
+}
+
+export const UnifiedLink: React.FC<UnifiedLinkProps> = ({
+    to,
+    children,
+    target = "",
+    className,
+}) => {
+    const Component = (to && isExternalLink(to)) || !!target ? "a" : Link;
+
+    return Component === "a" ? (
+        <Component className={className} target={target} href={to}>
+            {children}
+        </Component>
+    ) : (
+        <Component to={to} className={className}>
+            {children}
+        </Component>
+    );
+};
