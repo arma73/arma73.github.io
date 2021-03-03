b40d27a24788bf6f3d65530658bbdd66fecff9b7
arma73 <arma.biz9805@gmail.com>
2021-03-03 15:56:55 +0400

    add: with base modal skills

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -1,5 +1,7 @@
 import { useHover } from "_hooks/useHover";
+import { useToggle } from "_hooks/useToggle";
 import Section from "_components/form/Section";
+import { ShowSkills } from "_components/showcase";
 import { RiseAnimated } from "_components/animated";
 import Tooltip from "_components/tooltip";
 
@@ -7,20 +9,25 @@ import "./Skills.scss";
 
 const Skills = () => {
     const [isHovered, bind] = useHover();
+    const { isToggled, negativeToggle, positiveToggle } = useToggle();
 
     return (
-        <Section className="skills">
-            <div className="box--skills" {...bind}>
-                <RiseAnimated toX="-4px" toY="-4px">
-                    <div className="circles">
-                        <div className="circle-top preserve-3d"></div>
-                        <div className="circle-top preserve-3d"></div>
-                        <div className="circle-top"></div>
-                    </div>
-                </RiseAnimated>
-            </div>
-            <Tooltip show={isHovered} text="Skills" />
-        </Section>
+        <>
+            <Section className="skills">
+                <div className="box--skills" {...bind}>
+                    <RiseAnimated toX="-4px" toY="-4px">
+                        <div className="circles" onClick={positiveToggle}>
+                            <div className="circle-top preserve-3d"></div>
+                            <div className="circle-top preserve-3d"></div>
+                            <div className="circle-top"></div>
+                        </div>
+                    </RiseAnimated>
+                </div>
+                <Tooltip show={isHovered} text="Skills" />
+            </Section>
+            <ShowSkills onClose={negativeToggle} visible={isToggled} />
+        </>
     );
 };
+
 export default Skills;
