4a26041965aa8c8492f0ceecdc1d5118ef6c39e6
arma73 <arma.biz9805@gmail.com>
2021-03-03 17:36:12 +0400

    update: with showPassion modal

--- src/components/passions/Passions.tsx
+++ src/components/passions/Passions.tsx
@@ -1,5 +1,7 @@
 import { useHover } from "_hooks/useHover";
+import { useToggle } from "_hooks/useToggle";
 import { RiseAnimated } from "_components/animated";
+import ShowPassions from "_components/showcase/sh_passions";
 import Section from "_components/form/Section";
 import Tooltip from "_components/tooltip";
 
@@ -7,16 +9,24 @@ import "./Passions.scss";
 
 const Passions = () => {
     const [isHovered, bind] = useHover();
+    const { isToggled, negativeToggle, positiveToggle } = useToggle();
 
     return (
-        <Section className="passions">
-            <div className="box--passions" {...bind}>
-                <RiseAnimated toX="-4px" toY="-4px">
-                    <div className="bar--passions" />
-                </RiseAnimated>
-            </div>
-            <Tooltip show={isHovered} text="Passions" />
-        </Section>
+        <>
+            <Section className="passions">
+                <div
+                    className="box--passions"
+                    onClick={positiveToggle}
+                    {...bind}
+                >
+                    <RiseAnimated toX="-4px" toY="-4px">
+                        <div className="bar--passions" />
+                    </RiseAnimated>
+                </div>
+                <Tooltip show={isHovered} text="Passions" />
+            </Section>
+            <ShowPassions onClose={negativeToggle} visible={isToggled} />
+        </>
     );
 };
 
