6d083e63b30f860bb9a4689f1c6b02ee4ff84c7d
arma73 <arma.biz9805@gmail.com>
2021-03-06 12:36:02 +0400

    update: with education tooltip

--- src/components/education/Education.tsx
+++ src/components/education/Education.tsx
@@ -1,16 +1,23 @@
 import StraightLine from "_components/straightline";
+import Tooltip from "_components/tooltip";
+import { useHover } from "_hooks/useHover";
 
 import "./Education.scss";
 
-const Education = () => (
-    <div className="education">
-        <StraightLine
-            options={{
-                "short": true,
-            }}
-            lines={4}
-        />
-    </div>
-);
+const Education = () => {
+    const [isHovered, bind] = useHover();
+
+    return (
+        <div className="education" {...bind}>
+            <StraightLine
+                options={{
+                    "short": true,
+                }}
+                lines={4}
+            />
+            <Tooltip show={isHovered} text="Education" />
+        </div>
+    );
+};
 
 export default Education;
