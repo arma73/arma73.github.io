8630df67a0b0e121881af451d72acee1f2711d47
arma73 <arma.biz9805@gmail.com>
2021-03-06 12:03:13 +0400

    update: with hover tooltip

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -1,23 +1,31 @@
 import { useHistory } from "react-router-dom";
+import { useHover } from "_hooks/useHover";
 import { RiseAnimated } from "_components/animated";
 import { RoutePage } from "_settings/path.routes";
 import Square from "_components/shapes/square";
+import Tooltip from "_components/tooltip";
 
 import "./Experience.scss";
 
 const Experience = () => {
     const history = useHistory();
+    const [isHovered, bind] = useHover();
 
     const handleRoute = () => history.push(RoutePage.EXPERIENCE);
 
     return (
-        <RiseAnimated toX="-4px" toY="-4px">
-            <div className="experience" onClick={handleRoute}>
-                <Square className="connector" />
-                <Square className="connector" />
-                <Square />
+        <div className="experience">
+            <div {...bind} onClick={handleRoute}>
+                <RiseAnimated toX="-4px" toY="-4px">
+                    <div className="experience-shapes">
+                        <Square className="connector" />
+                        <Square className="connector" />
+                        <Square />
+                    </div>
+                </RiseAnimated>
             </div>
-        </RiseAnimated>
+            <Tooltip show={isHovered} text="Experience" />
+        </div>
     );
 };
 
