323d01c6fb5f25bfdfb765cdc8ee63a92fa74e77
arma73 <arma.biz9805@gmail.com>
2021-03-06 09:25:49 +0400

    update: to expand styled Text component

--- src/components/styled/Text.sc.ts
+++ src/components/styled/Text.sc.ts
@@ -1,10 +1,31 @@
 import styled from "styled-components";
 
-export const Text = styled.p`
+interface TextProps {
+    "size"?: string;
+    "color"?: string;
+    "weight"?: number;
+    "height"?: string;
+}
+
+export const Text = styled.p.attrs(
+    ({
+        size = "18px",
+        color = "#5e6788",
+        height = "1.5em",
+        weight = 500,
+        ...props
+    }: TextProps) => ({
+        ...props,
+        size,
+        color,
+        height,
+        weight,
+    })
+)`
     margin-top: 1em;
-    font-size: 18px;
-    font-weight: 500;
-    color: #5e6788;
+    font-size: ${({ size }) => size};
+    font-weight: ${({ weight }) => weight};
+    color: ${({ color }) => color};
     text-align: left;
-    line-height: 1.5em;
+    line-height: ${({ height }) => height};
 `;
